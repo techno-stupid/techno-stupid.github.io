357fb0c8c7f71a26e8d8a20fc942290cd18026a9
Mahady Hasan Pial <pial.coder@gmail.com>
2023-09-09 00:31:39 +0600

    Personal Info added

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'techno-stupid', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
@@ -11,58 +11,70 @@ const config = {
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'mhpial',
+    twitter: 'TechnoStupidOne',
+    mastodon: '',
     facebook: '',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '@pial.coder',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    website: 'https://technostupid.com',
+    phone: '+880 1735311153',
+    email: 'pial.coder@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://technostupid.com/doc/Md.%20Mahady_Hasan.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
     'Laravel',
     'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
+    'Golang',
     'MySQL',
     'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'AWS',
+    'Microservices',
+    'CICD',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Easital Technologies Ltd.',
+      position: 'Lead Software Engineer',
+      from: 'September 2022',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://easital.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Sheba Platform',
+      position: 'Lead Backend Engineer',
+      from: 'August 2019',
+      to: 'August 2022',
+      companyLink: 'https://www.sheba.xyz',
     },
+    {
+      company: 'WebAble Digital',
+      position: 'Software Engineer',
+      from: 'July 2018',
+      to: 'Jusy 2019',
+      companyLink: 'https://webable.digital',
+    },
+    {
+      company: 'Next IT Ltd.',
+      position: 'Software Engineer',
+      from: 'July 2017',
+      to: 'Jusy 2018',
+      companyLink: 'http://nextitltd.com',
+    },
+
   ],
   /* certifications: [
     {
@@ -74,40 +86,46 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Daffodin International University',
+      degree: 'B.Sc in CSE',
+      from: '2013',
+      to: '2016',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Govt. Syed Hatem Ali Collece',
+      degree: 'HSC',
+      from: '2010',
+      to: '2012',
+    },
+    {
+      institution: 'Barishal Zilla School',
+      degree: 'SSC',
+      from: '2009',
+      to: '2009',
     },
   ],
 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: 'Beeda Mega App',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'Beeda offers a one-stop solution for all your daily needs. With just a tap, you can access a vast array of services.',
+      imageUrl: 'https://res.cloudinary.com/djvdfx07x/image/upload/v1694197697/1_Cgv7zFJhiiqodvCCs3Du2A_q35se2.jpg',
+      link: 'https://beeda.com',
     },
     {
       title: 'Project Name',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+        'Sheba.xyz, largest service marketplace & one-stop solution for your home services in Bangladesh.',
+      imageUrl: 'https://play-lh.googleusercontent.com/KdD-PttwhTyD3ALaL-pW0t5sjymog-RALwhoxh0ee1K8rXDaIVZvCcw8ToE22UYGNDQ',
+      link: 'https://www.sheba.xyz',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: 'medium', // medium | dev
+    username: '@pial.coder', // to hide blog section, keep it empty
     limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
